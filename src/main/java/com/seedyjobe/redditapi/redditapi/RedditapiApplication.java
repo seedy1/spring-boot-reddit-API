7af0a9b44de8d6c77b943e34cff4a4e979190672
Seedy JOBE <seedy.reliefapps@gmail.com>
2022-12-01 15:43:09 +0100

    first commit

---
+++ src/main/java/com/seedyjobe/redditapi/redditapi/RedditapiApplication.java
@@ -0,0 +1,13 @@
+package com.seedyjobe.redditapi.redditapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seedyjobe/redditapi/redditapi/RedditapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seedyjobe.redditapi.redditapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
